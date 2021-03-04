c4a637880c3013b27d6f385da43013a247d475ab
Felicia Wootton <felicia.wootton@gmail.com>
2021-03-04 17:46:29 -0600

    Added contenteditable to html

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,6 @@
 const currentDate = document.getElementById("currentDay");
+const currentHour = moment().hour();
+
 
 function displayDate() {
     const todayDate = moment().format('MMMM DD, YYYY');
@@ -7,21 +9,23 @@ function displayDate() {
 };
 
 displayDate();
+// console.log(currentHour);
 
 
-
-// function setCurrentDate () {
-//     let currentMoment = moment().format('MMMM Do YYYY');
-//     currentDate.innerHTML = "<p>" + currentMoment + "<p>";
-// }
-
-// setCurrentDate();
 
 function setCurrentTimeBlock () {
+    const hour = document.getElementsByClassName("hour");
+    if (currentHour == 17) {
+        console.log("It's working");
+    };
     // if moment() === present, then background color red
+    // if the current time is between the timeblock-time + timeblock-time + 59 minutes and 59 seconds, then its green
+
     // if moment () > present, its the past, then background color gray and disable?
     // if moment () < presenttime, its the future, then background color green
 }
+
+setCurrentTimeBlock();
 
 
 function saveNewContent () {
