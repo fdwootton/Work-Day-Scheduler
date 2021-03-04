8f9858b823b1437fd43cf289f69249f52ab4a704
Felicia Wootton <felicia.wootton@gmail.com>
2021-03-04 16:29:14 -0600

    Added displayDate function to script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,21 @@
-function setCurrentDate () {}
-    // set the current date
+const currentDate = document.getElementById("currentDay");
+
+function displayDate() {
+    const todayDate = moment().format('MMMM DD, YYYY');
+    const dayoftheWeek = moment().format('dddd');
+    currentDate.textContent = todayDate + " - " + dayoftheWeek;
+};
+
+displayDate();
+
+
+
+// function setCurrentDate () {
+//     let currentMoment = moment().format('MMMM Do YYYY');
+//     currentDate.innerHTML = "<p>" + currentMoment + "<p>";
+// }
+
+// setCurrentDate();
 
 function setCurrentTimeBlock () {
     // if moment() === present, then background color red
@@ -14,3 +30,5 @@ function saveNewContent () {
 
 
 // Add click event for save button, then call saveNewContent ()
+
+
