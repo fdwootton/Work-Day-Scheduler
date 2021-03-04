c13e4efe1bb821a75aed467eb7a4c29f6beedc40
Felicia Wootton <felicia.wootton@gmail.com>
2021-03-04 15:23:17 -0600

    Added pseudocode to script.js

---
+++ assets/js/script.js
@@ -0,0 +1,16 @@
+function setCurrentDate () {}
+    // set the current date
+
+function setCurrentTimeBlock () {
+    // if moment() === present, then background color red
+    // if moment () > present, its the past, then background color gray and disable?
+    // if moment () < presenttime, its the future, then background color green
+}
+
+
+function saveNewContent () {
+    // make user input the text content of the time block description box
+}
+
+
+// Add click event for save button, then call saveNewContent ()
